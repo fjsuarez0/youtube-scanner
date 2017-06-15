a6f876d1ebf5da768abfd0c1ddb4dc917887fe9f
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-06-14 21:05:44 -0400

    Upload project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const exphbs = require("express-handlebars")
+const port = 8989
+const google = require("googleapis")
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')  
+
+app.set('views', path.join(__dirname, 'views')) 
+
+app.get('/', (req, res) => {  
+  res.render('home', {
+    name: 'John'
+  })
+})
+
+app.listen(port, () => {
+	console.log(`Servidor corriendo en el puerto ${port}`)
+})
