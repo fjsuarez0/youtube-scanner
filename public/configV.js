aaf707d8f34d02ecbb54adc04c4614399856a979
Francisco Suárez <fjsuarez@urbe.edu.ve>
2017-06-30 16:29:46 -0400

    Change variable name

--- public/configV.js
+++ public/configV.js
@@ -1,12 +1,10 @@
-var config = { 
+var configV = { 
 	google: {
 		client_id: '122389199276-ut2naq56p7gnf14qrjlsml5mhbs5djm5.apps.googleusercontent.com',	
 		api_key: 'AIzaSyByEs52_P--XiS4yKtX_QRfgMiEXjHNb8U',
 		hosted_domain: 'https://youtube-scanner.herokuapp.com',
 		scope: 'https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/yt-analytics.readonly',
-		discovery_docs: [	      
-         'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
-		],	
+		discovery_docs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],	
 	},
 	firebase: {
 	     apiKey: "AIzaSyCHKCMPwLkFpQDXwD997u1Fv6cCXBqbt90",
